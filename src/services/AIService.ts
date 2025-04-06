0d460a771389705f2a95c51f0d9bb2de1fa86431
parisollie <pjff18@gmail.com>
2025-04-06 11:46:37 -0600

    final ai

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -9,7 +9,7 @@ export default {
         const result = streamText({
             //V-352,Paso 8.18,Le pasmos el modelo
             model: openrouter('meta-llama/llama-3.3-70b-instruct:free'),
-            // model: openrouter('google/gemini-2.5-pro-exp-03-25:free'),
+            //model: openrouter('google/gemini-2.5-pro-exp-03-25:free'),
             // model: openrouter('deepseek/deepseek-chat-v3-0324:free'),
             // model: openrouter('google/gemma-3-4b-it:free'),
             //prompt
